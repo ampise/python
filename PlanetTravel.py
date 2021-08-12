0877e1973b5e5da91dac1bf58b718bc19a6e7590
Mangesh Pise <mppise@gmail.com>
2021-08-12 09:10:39 -0400

    Cleaned up

--- PlanetTravel.py
+++ PlanetTravel.py
@@ -2,3 +2,5 @@ planets = ["Mercury", "Venus", "Mars",
            "Jupiter", "Saturn", "Uranus", "Neptune"]
 planetDistances = [56974146, 25724767,
                    48678219, 390674710, 792248270, 1692662530, 2703959960]
+
+# TBD
