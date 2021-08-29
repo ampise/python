6c5ad736202ecc5617e784a77d580d422b19a9c3
Ananya Pise <piseananya@gmail.com>
2021-08-29 13:47:24 -0400

    Added comment.

--- temperature-convertor.py
+++ temperature-convertor.py
@@ -2,6 +2,7 @@ import os
 os.system("clear")
 
 celsius = float(input("Please enter a celsius value that you wish to convert to fahrenheit: "))
+# Convert celsius to fahrenheit using formula
 fahrenheit = (celsius * 1.8) + 32
 
 print("Your new value in fahrenheit is " + str(fahrenheit))
