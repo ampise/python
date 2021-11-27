860961edb47da5742daf1a1d4bc3104cf312c643
Ananya Pise <piseananya@gmail.com>
2021-11-27 13:52:51 -0500

    Word first and last

--- word-first-last.py
+++ word-first-last.py
@@ -2,14 +2,13 @@ from __future__ import print_function
 import os, math
 os.system("clear")
 
-word = "MANGESH"
+word = "hello"
 wordMidpoint = int(len(word)/2)
 
 last = len(word)
 for f in range(0, wordMidpoint, 1):
     print(word[f], end=" ")
     last = last - 1
-    for r in range(last, last - 1, -1):
-        print(word[r], end=" ") 
+    print(word[last], end=" ") 
 if len(word) % 2:
     print(word[wordMidpoint])
