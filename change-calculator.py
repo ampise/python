aaf6686a65e8d4fe436012b1c914b2cd7b1d0a86
Ananya Pise <piseananya@gmail.com>
2022-01-16 16:34:01 -0500

    Change calculator.

--- change-calculator.py
+++ change-calculator.py
@@ -1,3 +1,30 @@
 import os, random
 os.system("clear")
 
+# generateBill
+def generateBill():
+    amount = random.randint(4500, 5000)
+    return amount / 100
+
+# calculateChange
+def calculateChange(bill):
+    change = 5000 - (bill * 100)
+    return change / 100
+
+# changeDenomination
+def changeDenomination(###):
+    coins = [25, 10, 5, 1]
+    while change > 0:
+        for i in coins:
+            if (change - i) >= 0:
+                change = change - i
+            do until change <= 0:
+                
+
+# billAmount = generateBill()
+# print(billAmount)
+
+# changeAmount = calculateChange(billAmount)
+# print(changeAmount)
+
+# print("test ", billAmount + changeAmount)
