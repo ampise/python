c72c5901703142031456df6a9b23935a6392a00c
Ananya Pise <piseananya@gmail.com>
2022-06-15 17:24:45 -0400

    trying new program

---
+++ prime-factors.py
@@ -0,0 +1,12 @@
+import os, math
+os.system("clear")
+
+# Objective: List the prime factors of any given number.
+
+def primeFactors(num):
+    while num != 0:
+        num = num / 2
+    print(num)
+    
+    for i in range(2, i + 1):
+        
