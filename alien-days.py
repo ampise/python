525e3c6d78c179ff0931931ebfb6b2b6e751ca5b
Ananya Pise <piseananya@gmail.com>
2022-06-16 09:04:55 -0400

    prime factor project

---
+++ alien-days.py
@@ -0,0 +1,20 @@
+import os
+os.system("clear")
+
+# aLand - When did the aliens land? How many days ago?
+# eDay - Earth day.
+# aDay - Alien day.
+# Output: Next aDay with 24 hours.
+# Input: eDays ago batch of aliens came.
+# Every 12 days alien clock reset to 24 hours, every day increment 1 hour for 6 days and decrement for rest 6.
+
+aLand = int(print(input("How many days ago did the aliens land?: ")))
+
+def getInput(eDay):
+    ...
+    return
+
+def calcEqualDay(getInput):
+    return 12 - aLand
+
+for 
--- prime-factors.py
+++ prime-factors.py
@@ -3,10 +3,4 @@ os.system("clear")
 
 # Objective: List the prime factors of any given number.
 
-def primeFactors(num):
-    while num != 0:
-        num = num / 2
-    print(num)
-    
-    for i in range(2, i + 1):
-        
+
