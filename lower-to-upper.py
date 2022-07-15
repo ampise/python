0fb6c4a5eb01d37c32b0cd3d6b1afb6515d9b848
Mangesh Pise <mppise@gmail.com>
2022-07-14 22:24:57 -0400

    Morse Code

---
+++ lower-to-upper.py
@@ -0,0 +1,5 @@
+import os
+os.system("clear")
+
+def lowerToUpper():
+    CASE_DICT = {'a': 'A', 'b': 'B','c': 'C','d': 'D','e': 'E','f': 'F','g': 'G','h': 'H','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','a': 'A','z': 'Z',}
